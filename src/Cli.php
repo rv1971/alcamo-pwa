f73b2213934f0961a5abf32be1f4b9a999e055cb
RV1971 <rv1971@web.de>
2022-05-31 21:52:24 +0200

    Eliminate AbstractCli

--- src/Cli.php
+++ src/Cli.php
@@ -2,6 +2,8 @@
 
 namespace alcamo\pwa;
 
+use alcamo\cli\AbstractCli;
+
 class Cli extends AbstractCli
 {
     public const COMMANDS = [
@@ -12,6 +14,32 @@ class Cli extends AbstractCli
             'Setup the database'
         ]
     ];
+
+    protected $accountMgr_;
+
+    public function __construct(iterable $params)
+    {
+        parent::__construct();
+
+        $this->accountMgr_ = AccountMgr::newFromParams($params);
+    }
+
+    public function getAccountMgr(): AccountMgr
+    {
+        return $this->accountMgr_;
+    }
+
+    public function process($arguments = null): int
+    {
+        parent::process($arguments);
+
+        if (!$this->getCommand()) {
+            $this->showHelp();
+            return 0;
+        }
+
+        return $this->{$this->getCommand()->getHandler()}();
+    }
 
     public function setupDatabase(): int
     {
